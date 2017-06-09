567d756d77697a0b75434b296fbf256a56c8082c
Kalpak Take <kalpaktake@gmail.com>
2017-06-09 22:36:22 +0530

    output strings using echo print

---
+++ Syntax/outputing_strings.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+	<html lang="en-US">
+		<head>
+			<meta charset="utf-8">
+			<title>Example</title>
+			<meta name="author", content="Kalpak Take">
+		</head>
+
+		<body>
+			<p>
+				<h1>Outputing Strings in PHP.</h1>
+			</p>
+
+			<?php
+	
+				echo "Hello, World <br />";
+				print "<strong> This is strong text! </strong>"; // It is said that print is slower than echo method
+
+			?>
+
+
+		</body>
+	
+	</html>>
