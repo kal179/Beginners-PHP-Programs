a813857a854c8c9d77f6dcabdd3f67bee6a0fa58
Kalpak Take <kalpaktake@gmail.com>
2017-06-09 22:40:20 +0530

    datatypes supported in php

---
+++ Syntax/datatypes.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+		<head>
+			<meta charset="utf-8">
+			<title>Example</title>
+			<meta name="author", content="Kalpak Take">
+		</head>
+
+		<body>
+			<p>
+				<h1 style="color:rgb(83, 128, 172); font-family:courier; font-size:45px; text-align:left;">Datatypes avaliable in hypertext preprocessor!</h1>
+			</p>
+
+			<?php
+				/*
+					Php can store many datatypes as follows :
+				*/
+				$string = "Series of character/s";    // string is just a series of characters between parenthesis
+				$boolean = true;     // or false
+				$integer = 2420;     //negative positive numbers
+				$float = 3.14159;     // decimal point numbers
+				$nullType = NULL;      // null is none type obj of php
+				$array = array(1, 1, 2, 3, 5, 8, 13, 21, true, "fibonacci");    // array is indexed sequence of values
+				$assocArray = array(
+									
+									"Python" => "Guido Van Rossum",
+									"PHP" => "Rasmus Lerdorf",
+									"C++" => "Bjarne Stroustroup",
+									"Java" => "James Gosling",
+									"Ruby" => "Matz",
+									"C" => "Dennis Richtie"
+									
+									);       // also called map or dictionary, stores array in 'key' => 'value' pair
+				/*
+					It can also store new objects like following
+					$object = new Foo();
+				*/
+
+			?>
+
+		</body>
+	
+	</html>>
