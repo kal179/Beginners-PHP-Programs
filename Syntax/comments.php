8fd7557a33c0db8e5400290ce9344dcbfc9e2b4f
Kalpak Take <kalpaktake@gmail.com>
2017-06-09 22:37:05 +0530

    comments in php

---
+++ Syntax/comments.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+		<head>
+			<meta charset="utf-8">
+			<title>Example</title>
+			<meta name="author", content="Kalpak Take">
+		</head>
+
+		<body>
+			<p>
+				<h1 style="color:rgb(255, 128, 0); font-family:courier; font-size:40px; text-align:left;">Comments are cool!</h1>
+			</p>
+
+			<?php
+				// This is single line comment
+				//  Comments are very useful to remember the tasks and store documentation
+				// You can store multiline comments too!
+				// Just like this
+
+				/*
+					This is multiline comment
+				*/
+
+				/*
+					If u dont want a piece of script for some time u can comment it
+					and whenever u want u can un-comment it
+				*/
+
+				// Just like this
+
+				/*
+					$pi = 3.14159;
+					$radius = 24.2;
+					$circumference = 2 * $pi * $radius;
+					echo sprintf("<quote> Circumference of circle is = %f cm</quote>", $circumference);
+				*/
+
+				// This php script is not going to return anything unless you un-comment the upper multiline comment that contains script
+			?>
+
+		</body>
+	
+	</html>>
