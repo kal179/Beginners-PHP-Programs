48d23533875901c9218eecd28cab1b0a7fec48ac
Kalpak Take <kalpaktake@gmail.com>
2017-06-11 14:15:43 +0530

    variable scope in php

--- Syntax/variable_scope.php
+++ Syntax/variable_scope.php
@@ -8,7 +8,7 @@
 
 		<body>
 			<p>
-				<h1 style="color:rgb(173, 51, 255); font-family:courier; font-size:45px; text-align:left;">Global vs Local</h1>
+				<h1 style="color:rgb(173, 51, 255); font-family:courier; font-size:45px; text-align:left;">Global vs Local and Static</h1>
 			</p>
 
 			<?php
@@ -24,7 +24,24 @@
 					return "Global addition : ".$variable;     // This is another method
 				}
 
-				echo foo(), "<br/>";
+				echo foo(), "<br/><br/>";
+
+				/*
+					The values of variables in functions are normally deleted after using function 
+					But, for sake of recursion functions the static keyword is used,
+					it basically saves the state of variable after function is completed
+					Like in following example:
+				*/
+				function recursion(){
+					static $n = 0;
+					if ($n < 10){
+					    $n++;
+					    echo $n, "<br/>";
+					    recursion();
+					}
+				}
+
+				echo recursion();				
 			?>
 
 
