2cf5f80fe856879ca5c5a05f6b5bfdd2332a19cd
Kalpak Take <kalpaktake@gmail.com>
2017-06-11 14:40:21 +0530

    assignment ops in php

---
+++ Syntax/assignment_operators.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+		<head>
+			<meta charset="utf-8">
+			<title>Example</title>
+			<meta name="author", content="Kalpak Take">
+		</head>
+
+		<body>
+			<p>
+				<h1 style="color:rgb(173, 51, 255); font-family:courier; font-size:45px; text-align:left;">Assignment Ops</h1>
+			</p>
+
+			<?php
+				
+				// Assignment Operators
+				// '=' assignment operator used to declare variables and constants
+				// '+=' for updating variables value by adding itself plus new assigned number
+				// '-=' for updating variables value by subtracting from new assigned number
+				// '*=' for updating variables value by  multiplying itself into new assigned number
+				// '/=' for updating variables value by dividing itself by new assigned number
+				// '%=' for updating variables value by getting remainder by dividing value of variable by new assigned number
+				// '**=' for updating variables value by exponentiation of value into new assigned number
+
+				$n = 2020;
+
+				$n += 4;			// same as $n = $n + 4;
+				echo "<strong style='font-size:28px'>".$n."</strong>", "<br/>";  
+				$n -= 2000;  		// same as $n = $n - 2000;
+				echo "<strong style='font-size:28px'>".$n."</strong>", "<br/>";
+				$n *= 100;   		// same as $n = $n * 100;
+				echo "<strong style='font-size:28px'>".$n."</strong>", "<br/>";
+				$n /= 20; 		// same as $n = $n / 20; 
+				echo "<strong style='font-size:28px'>".$n."</strong>", "<br/>";
+				$n %= 9;		// same as $n = $n % 9;
+				echo "<strong style='font-size:28px'>".$n."</strong>", "<br/>";
+				$n **= 3;		// same as $n = $n ** 3;
+				echo "<strong style='font-size:28px'>".$n."</strong>", "<br/>";  
+				// value is probably 27
+
+			?>
+
+
+		</body>
+	
+	</html>
