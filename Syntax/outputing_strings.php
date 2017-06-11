f8446edee19a3ef1aca534bf716ab509d4b4393d
Kalpak Take <kalpaktake@gmail.com>
2017-06-11 14:55:26 +0530

    output strings in php

--- Syntax/outputing_strings.php
+++ Syntax/outputing_strings.php
@@ -16,11 +16,14 @@
 	
 				echo "Hello, World <br />";
 				print "<strong> This is strong text! </strong><br/>"; // It is said that print is slower than echo method
-				echo sprintf("Hello, %s, You are %d years old! isn't it!", "Kalpak", 15); 
+				echo sprintf("Hello, %s, You are %d years old! isn't it!", "Kalpak", 15), "<br/>"; 
 				/*
 				sprintf is used for string formatting %s for string %d for integer %f for float 
 				and more options are avaliable for formatting!!
-				*/
+				*/		
+				print "This"." is"." called"." concatenation!";    // . is concatenation operator 
+				// In other languages concatenation operator is normally + but in php it is .
+				
 
 			?>
 
