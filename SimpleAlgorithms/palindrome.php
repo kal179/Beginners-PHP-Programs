ec34c59e4296c00286cd658935cbe7bb1fb952b4
Kalpak Take <kalpaktake@gmail.com>
2017-06-12 23:24:14 +0530

    Fix string case

--- SimpleAlgorithms/palindrome.php
+++ SimpleAlgorithms/palindrome.php
@@ -1,18 +1,22 @@
 <?php
-	
+
 	function isPalindrome($s){
+		$s = strtolower($s);
 		$arr = array_reverse(str_split($s));
 		$toCompare = "";
 		for ($i = 0; $i < count($arr); ++$i){
-	        $toCompare .= $arr[$i];
-	    }
-	    if ($s == $toCompare){
-	        return "String is a palindrome!";
-	    }
+		    $toCompare .= $arr[$i];
+		}
+		if ($s == $toCompare){
+		    return "String is a palindrome!";
+		}
 		return "Sorry! it isn't a palindrome!";
 	}
 	
-	echo isPalindrome("noon"), "\n<br/>";
-
+	// remove \n or <br/> according to programming enviornmtemt you are using
 	
+	echo isPalindrome("noon"), "\n<br/>";    // should be a palindrome
+	echo isPalindrome("NoOn"), "\n<br/>";    // should be a palindrome
+	echo isPalindrome("racEcar"), "\n<br/>";   // should be a palindrome
+	echo  isPalindrome("kalpak"), "\n<br/>";   // is not a palindrome
 ?>
