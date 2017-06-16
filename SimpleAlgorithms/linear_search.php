8be3c3cbda17c2ce7d93a6d17a2a6058f38b4993
Kalpak Take <kalpaktake@gmail.com>
2017-06-17 00:28:57 +0530

    Fix testcase and output

--- SimpleAlgorithms/linear_search.php
+++ SimpleAlgorithms/linear_search.php
@@ -4,7 +4,7 @@
 
 	/*
 
-		Arguably this is the most easiest/simplest algorithm out there for searching.
+		Arguably this is the most easiest/simplest algorithm out there.
 		You just loop through the array! 
 		if target found 
 		return its position(index) as integer
@@ -29,7 +29,7 @@
 
 	$testCase = [123, 4565, 34576, 78637856, 38274, 3467, 4.655, 2467.23490, 235, 3.141509, 213878, 12344, -1223, -1233, 90.00000, 0.333333, 2, 54, 324, 76, 345, 6, 2412, 45456745, -134, 3457665, -12.5443, 9, 4, 123, 532,6, 90, 60, -1233.00];    // change values accordingly
 
-	$test=findIndexSearch($testCase, -3.141509);
+	$test=findIndexSearch($testCase, 9);
 	echo "Algorithm result : ".$test, "\n";        // As always change \n to <br/> if using browser
-	echo "Expected Results : ".array_search(3.141509, $testCase), "\n";    
+	echo "Expected Results : ".array_search(9, $testCase), "\n";   // change the value of first parameter corresponding to $test variable   
 ?>
