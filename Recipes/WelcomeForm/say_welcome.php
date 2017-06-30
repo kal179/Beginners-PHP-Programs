99613ecd307c72b730712a8a137fb551609e4bda
Kalpak Take <kalpaktake@gmail.com>
2017-06-30 22:50:38 +0530

    Update more readable

--- Recipes/WelcomeForm/say_welcome.php
+++ Recipes/WelcomeForm/say_welcome.php
@@ -7,11 +7,15 @@
 
 		<body>
 			<h1 style="font-size: 50px; font-family: verdana; text-align: center; color: rgb(255, 102, 102)">
-				<b >Welcome! </b>	<?php echo "<b style='font-size: 50px; font-family: verdana; text-align: center; color: rgb(255, 102, 102)'>".$_POST["name"]."</b>";?>
-			</h1><br/><br/><br/>
+				<b >Welcome! </b>	
+				<?php echo "<b style='font-size: 50px; font-family: verdana; text-align: center; color: rgb(255, 102, 102)'>".$_POST["name"]."</b>";?>
+			</h1>
+			
+			<br/><br/><br/>
 
-			<h2><i style="font-size: 25px; font-family: courier new; text-align: left; color: rgb(102, 102, 102)">You entered with email address: </i>
-			<?php echo "<i style='font-size: 25px; font-family: courier new; text-align: left; color: rgb(102, 102, 102)'>".$_POST["email"]."</i>"; ?>
+			<h2>
+				<i style="font-size: 25px; font-family: courier new; text-align: left; color: rgb(102, 102, 102)">You entered with email address: </i>
+				<?php echo "<i style='font-size: 25px; font-family: courier new; text-align: left; color: rgb(102, 102, 102)'>".$_POST["email"]."</i>"; ?>
 			</h2>
 		</body>
 
